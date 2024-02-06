a937575578b4205f2442d3c718e3ffcd6a2a9328
Nirmal-99 <nirmal99@gmail.com>
2024-02-06 17:15:33 +0530

    2024.02.06 index.js file

--- index.js
+++ index.js
@@ -1,15 +1,70 @@
-import express from "express"
-import cors from "cors"
+const express = require("express");
+const http = require('http');
+const cors = require("cors");
+require("dotenv").config();
+const { APISignIn } = require("./api/token.js");
+const { _GetUserPlantList } = require("./api/functions.js");
+// const { initializeWebSocket } = require('./sockets/socketsHandler.js');
+const { insertDataIntoDatabase } = require('./database/getplants.js');
 
-import { signupRouter } from './routes/signup.js'
-import { loginRouter } from './routes/login.js'
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
 
-const app = express()
-app.use(express.json())
-app.use(cors())
-app.use('/', signupRouter)
-app.use('/', loginRouter)
+const signupRouter = require("./routes/signup.js");
+const loginRouter = require("./routes/login.js");
+
+const app = express();
+// const server = http.createServer(app);
+app.use(express.json());
+app.use(cors());
+app.use("/", signupRouter);
+app.use("/", loginRouter);
+
+
+const BaseURL= process.env.BaseURL
+const GetUserPlantList= process.env.GetUserPlantList
+const PORT = process.env.PORT || 3002;
+
+// Initialize WebSocket
+// initializeWebSocket(server);
 
 app.listen(3002, () => {
-    console.log("Server is running .....")
+  console.log(`Server is running on port --> ${PORT}`);
 })
+
+// Start the server
+// const PORT = process.env.PORT || 3002;
+// server.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+app.post("/", async (req, res) => {
+  await APISignIn();
+  res.send("Token Renewed")
+  console.log("Token Renewed")
+})
+
+app.get("/getplant", async (req, res) => {
+  try {
+    const _res = await _GetUserPlantList() 
+    res.json({ mydata: _res.data });
+    const table_name = "plants"
+    const primary_key = "plant_id"
+    await insertDataIntoDatabase(table_name, primary_key, _res.data.data.list)
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error });
+  }
+});
+
+
+app.get("/plants", async (req, res) => {
+  try {
+    const _res = await _GetUserPlantList();
+    res.json({ plants: _res.data.data.list });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+});
